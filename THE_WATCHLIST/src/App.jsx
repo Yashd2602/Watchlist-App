98f610700c81a6ea72e870786c66038c569efd0d
Yashd2602 <yashdubey2602@gmail.com>
2025-06-21 18:34:48 +0530

    pushing for deployment

--- THE_WATCHLIST/src/App.jsx
+++ THE_WATCHLIST/src/App.jsx
@@ -39,7 +39,7 @@ const router = createBrowserRouter([{
   path:"/watchlist",
   element:<WatchList />,
 },
-])
+],{basename: import.meta.env.DEV ? '/' : '/Watchlist-App'})
 
   return (
     <RouterProvider router={router}/>
