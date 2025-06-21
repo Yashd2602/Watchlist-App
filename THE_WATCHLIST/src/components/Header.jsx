67df24471aa9c1bca72ca26001543ffccfc0153a
Yashd2602 <yashdubey2602@gmail.com>
2025-06-22 02:43:13 +0530

    updated assests folder

--- THE_WATCHLIST/src/components/Header.jsx
+++ THE_WATCHLIST/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { cinemaState } from '../Context/Context';
+import file from "../assets/file.png"
 
 function Header() {
   return (
     <div className='Header'><h1>The~Watchlist</h1> 
-    <Link to="/"><img className='watchlist-icon' src="/img/file.png" alt="Watchlist" /></Link>
+    <Link to="/"><img className='watchlist-icon' src={file} alt="Watchlist" /></Link>
     <Link to='/watchlist'><button>WatchList</button></Link>
     <Link to= "/"><button>Explore More Movies & TV_Shows</button></Link>
     </div>
--- THE_WATCHLIST/src/components/Homepage.jsx
+++ THE_WATCHLIST/src/components/Homepage.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from 'react'
 import{Link} from 'react-router-dom'
 import Header from './Header'
 import { cinemaContext, cinemaState } from '../Context/Context'
+import file from "../assets/file.png"
+import bookmark from "../assets/bookmark.png"
 
 function Homepage() {
   const{genre,setGenre} = useContext(cinemaContext);
@@ -15,10 +17,10 @@ function Homepage() {
   return (
     <div className='container'>
       <div className='Header'><h1>The~Watchlist</h1> 
-          <Link to="/"><img className='watchlist-icon' src="/img/file.png" alt="Watchlist" /></Link>
+          <Link to="/"><img className='watchlist-icon' src={file} alt="Watchlist" /></Link>
           <Link to='/watchlist'><button>WatchList</button></Link></div>
       <div className='homepage'>
-       <Link to="/movies"><button onClick={getMovies}>Movies</button></Link> <img className='cinema-time' src="/img/bookmark.png" alt="" /><Link to="/tvshows"><button onClick={getTV}>TV Shows</button></Link>
+       <Link to="/movies"><button onClick={getMovies}>Movies</button></Link> <img className='cinema-time' src={bookmark} alt="" /><Link to="/tvshows"><button onClick={getTV}>TV Shows</button></Link>
       </div>
     </div>
   )
